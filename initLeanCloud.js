c1d491ff8c963a04376c1d120cca52231c2f088e
wjm <1186619054@qq.com>
2018-05-17 17:36:21 +0800

    测试demo

---
+++ initLeanCloud.js
@@ -0,0 +1,9 @@
+{
+  // 初始化 leanCloud 服务
+  let APP_ID = 'oAEzkRDKPClvGEqPrQcV9vHb-gzGzoHsz'
+  let APP_KEY = 'T4dIKUkQ2PDgY8o14JseBPUY'
+  AV.init({
+      appId: APP_ID,
+      appKey: APP_KEY
+  })
+}
